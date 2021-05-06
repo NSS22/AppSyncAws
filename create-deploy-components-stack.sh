6727cd8df697e61f806ef7b875258936e32e21c4
Serhii_Nehoda <snehoda@intellias.com>
2021-05-07 00:36:35 +0300

    fix functionality issues

--- create-deploy-components-stack.sh
+++ create-deploy-components-stack.sh
@@ -1,14 +1,14 @@
 aws cloudformation create-stack --stack-name dynamodb-materialised-view \
     --template-body file://resources/dynamodb.yaml \
     --capabilities CAPABILITY_NAMED_IAM
-aws cloudformation wait stack-cteate-complete --stack-name dynamodb-materialised-view
+aws cloudformation wait stack-create-complete --stack-name dynamodb-materialised-view
 
 aws cloudformation create-stack --stack-name appsync-resolver \
-    --template-body file://resources/schema/resolver.yaml \
+    --template-body file://resources/appsync/resolver.yaml \
     --capabilities CAPABILITY_NAMED_IAM
-aws cloudformation wait stack-cteate-complete --stack-name appsync-resolver
+aws cloudformation wait stack-create-complete --stack-name appsync-resolver
 
 aws cloudformation create-stack --stack-name resource-resolvers \
     --template-body file://resources/resolvers.yaml \
     --capabilities CAPABILITY_NAMED_IAM
-aws cloudformation wait stack-cteate-complete --stack-name resource-resolvers
+aws cloudformation wait stack-create-complete --stack-name resource-resolvers
